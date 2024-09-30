6c920c6033ccb807b01b3d00e3476761da27aff5
monukdjavadev <monukd01dev@gmail.com>
2024-10-01 04:14:37 +0530

    cart functionality and Redux

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,27 @@
 import { Link } from "react-router-dom";
 import { FF_LOGO } from "../../utils/constants";
+import { useSelector } from "react-redux";
+import FFL from "../assets/FFL.png";
 
 const Header = () => {
+	const cartItems = useSelector((store) => store.cart.items);
+
 	return (
 		<div className="header">
 			<div className="logo">
-				<img src={FF_LOGO} alt="flavor fusion logo" />
+				<img src={FFL} alt="flavor fusion logo" />
 			</div>
 			<div className="nav-items">
 				<Link to="/">Home</Link>
 				<Link to="/contact">Contact</Link>
 				<Link to="/about">About</Link>
 				<Link to="/cart">
-					<i className="fa-solid fa-cart-shopping" />
+					<div className="cart-icon-head">
+						<i className="fa-solid fa-cart-shopping" />
+						{cartItems.length !== 0 && (
+							<div className="cart-items">{cartItems.length}</div>
+						)}
+					</div>
 				</Link>
 			</div>
 		</div>
